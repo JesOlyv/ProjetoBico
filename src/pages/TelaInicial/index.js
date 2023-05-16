7d427292aa2f9e218a13ab36bcf85ae88b260c5f
JesOly <jessicab.oliveira1992@gmail.com>
2023-05-16 11:28:15 -0300

    inclusão .db

--- src/pages/TelaInicial/index.js
+++ src/pages/TelaInicial/index.js
@@ -1,33 +1,122 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, Alert, SafeAreaView, Text } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet,Image ,TextInput, TouchableOpacity } from 'react-native';
 
+import * as Animatable from 'react-native-animatable'
 
-
+import { useNavigation } from '@react-navigation/native';
 
 export default function TelaInicial() {
-    const [solicitacao, setSolicitacao] = useState('');
-    
-    // função para buscar a última solicitação de serviço do banco de dados
-    const buscarUltimaSolicitacao = async () => {
-        try {
-            const resultado = await db.executeSql('SELECT * FROM solicitacoes ORDER BY id DESC LIMIT 1');
-            const rows = resultado[0].rows;
-            if (rows.length > 0) {
-                const { cliente, descricao, data } = rows.item(0);
-                setSolicitacao(`Cliente: ${cliente}\nDescrição: ${descricao}\nData: ${data}`);
-            }
-        } catch (error) {
-            console.error(error);
-            Alert.alert('Erro ao buscar solicitação de serviço.');
-        }
-    };
-    
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 20 }}>Bem-vindo ao seu perfil: Profissional!</Text>
-            <Button title="Verificar solicitação de serviço" onPress={buscarUltimaSolicitacao} />
-            <Text style={{ marginTop: 20 }}>{solicitacao}</Text>
+    const navigation = useNavigation();
+    return(
+        
+        <View style={styles.container}>
+                   <View style={styles.containerLogo}>
+                <Animatable.Image
+                animation="flipInY"
+                source={require('../../assets/logoo.png')}
+                style={{ width: '40%'}}
+                resizeMode="contain"
+                />
+            </View>
+            <Animatable.View animation="fadeInLeft" delay={200} style={styles.containerHeader}> 
+                <Text style={styles.message}>Bem-vindo ao seu perfil: Profissional!</Text>
+                
+
+
+            </Animatable.View>
+
+            <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+                <Text style={styles.title}> </Text>
+                <TextInput
+                placeholder="Clique nas opções abaixo e comece!"
+                style={styles.input}
+                
+                />
+                
+
+
+<TouchableOpacity 
+                style={styles.button}
+                onPress={() => navigation.navigate('Solicitação')}
+                >
+                    <Text style={styles.buttonText}>Verificar a solicitação de serviços</Text>
+                                    
+                </TouchableOpacity>
+                
+               
+                
+              
+              
+                           
+                                     
+                    
+ 
+
+            </Animatable.View>
+            
         </View>
     );
-}
-  
+} 
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#38a69d'
+
+    },
+    containerLogo:{
+        flex:1,
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    containerHeader:{
+        marginTop: '5%',
+        marginBottom:'5%',
+        paddingStart: '5%',
+
+    },
+    message:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#FFF'
+    },
+    containerForm: {
+        backgroundColor: '#FFF',
+        flex:1,
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%'
+    },
+    title:{
+        fontSize: 8,
+        marginTop:28,
+    },
+    input:{
+        borderBottomWidth: 1,
+        heigth: 40,
+        marginBottom: 12,
+        fontSize: 16,
+    },
+    button:{
+        backgroundColor: '#38a69d',
+        width:'100%',
+        borderRadius: 4,
+        paddingVertical: 8,
+        marginTop: 14,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText:{
+        color: '#FFF',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    buttonRegister:{
+        marginTop: 14,
+        alignSelf: 'center'
+    },
+    registerText:{
+        color: '#38a69d'
+    }
+})
